7ceb46b26b3615185721e4260d4422a1020a5786
nathanPayson <66044402+nathanPayson@users.noreply.github.com>
2024-02-29 11:32:37 -0500

    Alteration

--- assignments/05_grid/sketch.js
+++ assignments/05_grid/sketch.js
@@ -45,22 +45,22 @@ function drawGrid() {
             let g = colorArray[3 * (xIndex * numCellsHeight + yIndex) + 1];
             let b = colorArray[3 * (xIndex * numCellsHeight + yIndex) + 2];
 
-            b = (b + colorOffset) % 255 + 30;
-            g = (g + colorOffset) % 255 + 150;
+            b = (b + colorOffset) % 225 + 30;
+            g = (g + colorOffset) % 105 + 150;
             r = (r + colorOffset) % 50;
 
             fill(color(r, g, b));
             rect(0, 0, cellWidth, cellHeight,map(noise(x*0.1+noiseOffset,y*0.1+noiseOffset),0,1,0,15));
             strokeWeight(1 + map(noise(x * 0.1+strokeOffset, y * 0.1+strokeOffset), 0, 1, 1, 10));
-            stroke(1);
+            //stroke(1);
             let r1 = lineArray[4 * (xIndex * numCellsHeight + yIndex)];
             let r2 = lineArray[4 * (xIndex * numCellsHeight + yIndex)+1];
             let r3 = lineArray[4 * (xIndex * numCellsHeight + yIndex)+2];
             let r4 = lineArray[4 * (xIndex * numCellsHeight + yIndex)+3];
-            line(r1, r2, r3, r4);
+            //line(r1, r2, r3, r4);
             pop();
         }
-        colorOffset += 0.008;
+        colorOffset += 0.02;
         noiseOffset += 0.0005;
         strokeOffset += 0.001;
     }
